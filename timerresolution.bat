ddf66040969924e048d6d688c5f34ce6f5cb9fd4
Mirgarr <95709058+Mirgarr@users.noreply.github.com>
2024-10-08 17:22:44 +0200

    downloads timerresolution.exe

--- timerresolution.bat
+++ timerresolution.bat
@@ -1,4 +1,9 @@
 @echo off
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit
+)
 setlocal
 set "url=https://github.com/Mirgarr/timerresolution/raw/main/TimerResolution.exe"
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
@@ -9,3 +14,4 @@ powershell -command "Invoke-WebRequest -Uri '%url%' -OutFile '%outputFile2%'"
 start "" "%outputFile2%"
 
 endlocal
+exit
